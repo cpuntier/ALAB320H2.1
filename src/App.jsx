71d7fea3d803c5d2bec98b7c9c812da4ac014fdf
cpuntier <cpuntier@gmail.com>
2024-01-29 20:53:27 -0500

    initial and final? commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function Header() {
+  return (<h1>Simple React Application</h1>);
+}
+
+function Content(props) {
+  return (<p style={{ color: props.color }}>{props.text}</p>);
+}
+
+function Footer() {
+  return (<h1>Created by Chris, of course.</h1>);
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content color="blue" text="This is my first React Application!" />
+      <Content color="red" text="Wish me luck..." />
+      <Content color="green" text="I think I've got it!" />
+      <Content color = "white" text = "This was not that bad. " />      
+      <Footer />
+    </>
+  );
+}
+
+export default App;
